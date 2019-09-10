565451789afb92de2fbf4574d838e3085113e888
Vlad Hrybok <vgribok@hotmail.com>
2019-09-10 15:57:40 -0400

    #15 Generated blank CDK C# project.

---
+++ infra-as-code/src/InfraAsCodeStack.cs
@@ -0,0 +1,12 @@
+using Amazon.CDK;
+
+namespace InfraAsCode
+{
+    public class InfraAsCodeStack : Stack
+    {
+        public InfraAsCodeStack(Construct parent, string id, IStackProps props) : base(parent, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
---
+++ infra-as-code/src/Program.cs
@@ -0,0 +1,17 @@
+﻿using Amazon.CDK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace InfraAsCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var app = new App(null);
+            new InfraAsCodeStack(app, "InfraAsCodeStack", new StackProps());
+            app.Synth();
+        }
+    }
+}
