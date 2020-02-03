906ce5d0639690b7c108978ba30f3d570eb2b199
Vlad Hrybok <vgribok@hotmail.com>
2020-02-02 23:27:02 -0500

    Removing old EC2 lab box init scripts due to new ones having their own GitHub repo at https://github.com/vgribok/AWS-EC2-Windows-Dev-Init

--- hugo-content/resources/code/vm-init-scripts/scripts/lab-assets-deletion.bat
+++
@@ -1,6 +0,0 @@
-:: This script is called from explicit lab cleanup link (c:\finish-lab.lnk)
-
-cd C:\Users\Administrator\InitScripts
-call C:\Users\Administrator\InitScripts\delete-credentials.bat
-call C:\Users\Administrator\InitScripts\delete-unicornstore-assets.bat
-call C:\Users\Administrator\InitScripts\undo-script.bat
--- hugo-content/resources/code/vm-init-scripts/scripts/password-reset.bat
+++
@@ -1,3 +0,0 @@
-:: This script is called from the 
-@echo off
-net user Administrator Passw0rd
